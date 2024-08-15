62d55f6b1c239a0a489b15b11d6fc02a5bd8f4a4
Le Saux Tristan <tristan.le-saux@etu.univ-orleans.fr>
2024-08-15 14:21:03 +0200

    Modification predeploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
+import { HashRouter, RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router}/>
+		<HashRouter>
+			<RouterProvider router={router} />
+		</HashRouter>
 	</StrictMode>,
 )
