d7c0e8134c7946893ca844f8622f36fa49dd1f7c
Le Saux Tristan <tristan.le-saux@etu.univ-orleans.fr>
2024-08-15 12:17:11 +0200

    Correction Error

--- src/Layouts/ErrorPage.tsx
+++ src/Layouts/ErrorPage.tsx
@@ -1,16 +1,10 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error:any = useRouteError();
+  const error = useRouteError();
   console.error(error);
 
   return (
-    <Flex id="error-page" direction={"column"}>
-      <Heading>Oops!</Heading>
-      <Heading>Sorry, an unexpected error has occurred.</Heading>
-      <Heading>
-        <i>{error.statusText || error.message}</i>
-      </Heading>
-    </Flex>
+    <></>
   );
 }
